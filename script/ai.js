9afe2efa4895d56b3eadd774475144a85f26a7be
Thea / Aesther <154717468+Josephstarlord@users.noreply.github.com>
2024-06-22 20:27:04 +0300

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,33 +1,56 @@
 const axios = require('axios');
-module.exports.config = {
-  name: 'ai',
-  version: '1.0.0',
-  role: 0,
-  hasPrefix: false,
-  aliases: ['gpt', 'ae'],
-  description: "An AI command powered by GPT-4",
-  usage: "Ai questions" ,
-  credits: 'aesther',
-  cooldown: 3,
+
+const fonts = {
+a: "𝖺", b: "𝖻", c: "𝖼", d: "𝖽", e: "𝖾", f: "𝖿", g: "𝗀", h: "𝗁", i: "𝗂",
+
+        j: "𝗃", k: "𝗄", l: "𝗅", m: "𝗆", n: "𝗇", o: "𝗈", p: "𝗉", q: "𝗊", r: "𝗋",
+
+        s: "𝗌", t: "𝗍", u: "𝗎", v: "𝗏", w: "𝗐", x: "𝗑", y: "𝗒", z: "𝗓",
+
+        A: "𝖠", B: "𝖡", C: "𝖢", D: "𝖣", E: "𝖤", F: "𝖥", G: "𝖦", H: "𝖧", I: "𝖨",
+
+        J: "𝖩", K: "𝖪", L: "𝖫", M: "𝖬", N: "𝖭", O: "𝖮", P: "𝖯", Q: "𝖰", R: "𝖱",
+        S: "𝖲", T: "𝖳", U: "𝖴", V: "𝖵", W: "𝖶", X: "𝖷", Y: "𝖸", Z: "𝖹",
+    }
 };
-module.exports.run = async function({
-  api,
-  event,
-  args
-}) {
-  const input = args.join(' ');
-  if (!input) {
-    api.sendMessage(`♡ ∩_∩\n  （„• ֊ •„)♡\n┏━∪∪━━━━ღ❦ღ┓\n🛜 [ Hello Boss? ]✨:\n┗ღ❦ღ━━━━━━━┛ `, event.threadID, event.messageID);
-    return;
-  }
-  api.sendMessage(``, event.threadID, event.messageID);
-  try {
-    const {
-      data
-    } = await axios.get(`https://soyeon-api.onrender.com/api?prompt=${encodeURIComponent(input)}`);
-    const response = data.response;
-    api.sendMessage('♡ ∩_∩\n  （„• ֊ •„)♡\n┏━∪∪━━━━ღ❦ღ┓\n' + response + '\n[📩]\n┗ღ❦ღ━━━━━━━┛ ', event.threadID, event.messageID);
-  } catch (error) {
-    api.sendMessage('⚠️ 🫠エラー。。。。。 ⚠️', event.threadID, event.messageID);
-  }
+
+module.exports.config = {
+    name: 'ai',
+    version: '2',
+    role: 0,
+    hasPrefix: false,
+    aliases: ['gpt', 'ae'],
+    description: "Command for any Questions styled ",
+    usage: "ex : ai [prompt]",
+    credits: 'aesther',
+    cooldown: 1,
+};
+
+module.exports.run = async function({ api, event, args }) {
+    const input = args.join(' ');
+    
+    if (!input) {
+        api.sendMessage(`🟢 ᗩEᔕTᕼEᖇ ⚪ `, event.threadID, event.messageID);
+        return;
+    }
+    
+    api.sendMessage(``, event.threadID, event.messageID);
+    
+    try {
+        const { data } = await axios.get(`https://soyeon-api.onrender.com/api?prompt=${encodeURIComponent(input)}`);
+        let response = data.response;
+        
+        // Replace characters with stylized characters from fonts.mathsans
+        response = response.split('').map(char => {
+            if (fonts.mathsans[char]) {
+                return fonts.mathsans[char];
+            } else {
+                return char;
+            }
+        }).join('');
+        
+        api.sendMessage('🟢 ᗩEᔕTᕼEᖇ ⚪\n' + response + ' 🟡', event.threadID, event.messageID);
+    } catch (error) {
+        api.sendMessage('⚠️Error Loading ⚠️', event.threadID, event.messageID);
+    }
 };
